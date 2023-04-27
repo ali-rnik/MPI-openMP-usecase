4d1e2919d54401ddba556dce59c598a820830e8a
Ali Ramezaniannik <ali.ramezaniannik@student.griffith.ie>
2023-04-27 20:09:40 +0100

    almost complete openmp example

--- main.cpp
+++ main.cpp
@@ -1,35 +1,45 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include <thread>
+#include <omp.h>
 
 using namespace std;
 
-#define ROWS 3
-#define COLUMNS 3
+#define ROWS 100
+#define COLUMNS 100
 #define DEG_FREEDOM 4
 
 void generate_matrix(bool print);
 bool in_border(int row, int col);
 bool check_pal_size_n_from_cell(int palindrome_size, int cell_row, int cell_col, int row_direction, int col_direction);
 int count_pal_all_dir(int pal_size, int cell_row, int cell_col);
-
 char matrix[ROWS][COLUMNS];
-
 int degree_of_freedom_row[DEG_FREEDOM] = {0, 1, 1, 1};   //right_left, up_down, diagonally up_down_left, diagonally up_down_right
 int degree_of_freedom_col[DEG_FREEDOM] = {-1, 0, -1, 1}; //right_left, up_down, diagonally up_down_left, diagonally up_down_right
 
 int main() 
 {
-  generate_matrix(true);
-
-  for (int k = 1; k < 4; k++) {
-    int cnt = 0;
-    for (int i = 0; i < ROWS; i++) {
-      for (int j = 0; j < COLUMNS; j++) {
-          cnt += count_pal_all_dir(k, i, j);
+  generate_matrix(false);
+  int processor_count = omp_get_num_procs();
+  cout << processor_count << " << processor count!" << endl;
+  omp_set_num_threads(4);
+  cout << omp_get_num_threads() << " << num threads " << endl;
+  for (int t = 1; t <= processor_count; t++) {
+    for (int k = 1; k < 20; k++) {
+      int cnt = 0;
+      double begin = omp_get_wtime();
+      #pragma omp parallel for reduction (+:cnt)
+      for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLUMNS; j++) {
+            cnt += count_pal_all_dir(k, i, j);
+        }
       }
+      double end = omp_get_wtime();
+      double time_spent = end - begin;
+      cout <<fixed<< cnt << " palindromes of size " << k << " found in " << time_spent << " s. using " << omp_get_num_threads() << " Thread(s)" << endl;
     }
-    cout << cnt << " palindromes of size " << k << " found in " << endl;
+    cout << "********************************************************************************" << endl;
   }
 }
 
